f956028c74c920c3607aca17b2d8a225206876da
Ankit Baghel <baghelankit602@gmail.com>
2026-04-22 23:39:51 +0530

    Route Middleware in Express JS

--- Learn-Express/index.js
+++ Learn-Express/index.js
@@ -2,46 +2,46 @@ import express from 'express';
 const app = express();
 
 
-// function ageCheck(req, resp, next){
-
-//     if(!req.query.age || req.query.age < 18 ){
-//         resp.send("<h1>Access Denied</h1>")
-//     } else {
-//         next();
-//     }
-
-// }
-
-// app.use(ageCheck);
-
-function ipCheck(req, resp, next){
-    const ip = req.socket.remoteAddress;
-    console.log(ip);
-    if(ip.includes('192.168.1.11')){
-        resp.send("<h1>Access Denied for this IP</h1>");
+function checkAgeRouteMiddleware(req, resp, next){
+    console.log(req.query.age);
+    
+    if(!req.query.age || req.query.age < 18){
+        resp.send("You are not allowed to used this website"); 
     }else {
-
         next();
     }
-    
-
-    
-
 }
 
-app.use(ipCheck);
+function checkURLRouteMiddleware(req, resp, next){
 
-app.get('/', (req, resp) => {
-resp.send("<h1>Home Page</h1>")
-})
+    console.log("This request url is", req.url);
+    
+    next();
+}
 
-app.get('/login', (req, resp) => {
-    resp.send("<h1>Login Page</h1>")
-})
 
-app.get('/admin', (req, resp) => {
-    resp.send("<h1>Admin Page</h1>")
-})
+
+app.get('',(req, resp) => {
+    resp.send("<h1>Home Page</h1>");
+});
+
+app.get('/login',checkURLRouteMiddleware,(req, resp) => {
+    resp.send("<h1>Login Page</h1>");
+});
+
+
+app.get('/users', checkAgeRouteMiddleware, checkURLRouteMiddleware, (req, resp) => {
+    resp.send("<h1>Users Page</h1>");
+});
+
+
+app.get('/products',checkAgeRouteMiddleware,checkURLRouteMiddleware,(req, resp) => {
+    resp.send("<h1>Products Page</h1>");
+});
+
+
+
+
 
 
 app.listen(3000);
