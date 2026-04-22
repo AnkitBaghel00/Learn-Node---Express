fcef577c088fc20793f42d09bb8c336c625c484b
Ankit Baghel <baghelankit602@gmail.com>
2026-04-23 00:24:29 +0530

    External Middleware in Express JS

--- Learn-Express/index.js
+++ Learn-Express/index.js
@@ -1,35 +1,17 @@
 import express from 'express';
-import path from 'path';
-const app = express();
+import morgan from 'morgan';
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.static('public'));
+const app = express();
+app.use(morgan('dev'));
 
 
 app.get('/', (req, resp) => {
-    const filePath = path.resolve('view/home.html');
-    resp.sendFile(filePath);
+     resp.send("Home page");
 })
 
+app.get("/users", (req, resp) => {
+    resp.send("Users page");
+});
 
-app.get('/login', (req, resp) => {
-    resp.send(`
-        <form action="/submit" method="post">
-        <input type="text" placeholder="enter email" name="email"/>
-        <input type="password" placeholder="enter password" name="password"/>
-        <button type="submit">login</button>
-        </form>
-        `);
-})
-
-app.post('/submit', (req, resp) => {
-    console.log("user login details are : ",req.body);
-    
-    resp.send("Submit Page");
-})
-
-app.get('/users', (req, resp) => {
-    resp.send("Users Page");
-})
 
 app.listen(3000);
